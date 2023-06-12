4a804aa78a7a03dbc989004e5a7ce005a0a8c8e1
Arth Vhanesa <77712031+ArthVhanesa@users.noreply.github.com>
2023-06-12 17:53:10 +0530

    Update Popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -3,19 +3,20 @@ const jethalalGifElement = document.getElementById("jethalal-gif");
 
 perfectVideoButton.addEventListener("click", openRandomVideo);
 
-const jethalalGif = [
-  "https://media.tenor.com/UUyL8mVNYvMAAAAC/jetha-jethalal.gif",
-  "https://media.tenor.com/tgDbJxp0Yt4AAAAC/jethalal-tarak.gif",
-  "https://media.tenor.com/q2D1TATYTY8AAAAC/jethalal-tmkoc.gif",
-  "https://media.tenor.com/rcUlT6pAH9MAAAAC/jethalal-sab-changa-c.gif",
-  "https://media.tenor.com/rNbOhgMCWOkAAAAC/jethalal-jethalal-dancing.gif",
-  "https://media.tenor.com/W06knTv2yOUAAAAd/jetha-dance.gif",
-  "https://media.tenor.com/jcsfoiV2df4AAAAd/jetha.gif",
-];
+// const jethalalGif = [
+//   "https://media.tenor.com/UUyL8mVNYvMAAAAC/jetha-jethalal.gif",
+//   "https://media.tenor.com/tgDbJxp0Yt4AAAAC/jethalal-tarak.gif",
+//   "https://media.tenor.com/q2D1TATYTY8AAAAC/jethalal-tmkoc.gif",
+//   "https://media.tenor.com/rcUlT6pAH9MAAAAC/jethalal-sab-changa-c.gif",
+//   "https://media.tenor.com/rNbOhgMCWOkAAAAC/jethalal-jethalal-dancing.gif",
+//   "https://media.tenor.com/W06knTv2yOUAAAAd/jetha-dance.gif",
+//   "https://media.tenor.com/jcsfoiV2df4AAAAd/jetha.gif",
+// ];
 
-const randomIndex = Math.floor(Math.random() * jethalalGif.length);
+// const randomIndex = Math.floor(Math.random() * jethalalGif.length);
 
-jethalalGifElement.src = jethalalGif[randomIndex];
+// jethalalGifElement.src = jethalalGif[randomIndex];
+
 // console.log("hello popup.js");
 
 function openRandomVideo() {
@@ -27,7 +28,7 @@ function openRandomVideo() {
       const randomVideoID = episodes[randomIndex].youtubeVideoID;
 
       const youtubeURL = `https://www.youtube.com/watch?v=${randomVideoID}`;
-      // chrome.tabs.create({ url: youtubeURL });
+      chrome.tabs.create({ url: youtubeURL });
     }
   });
 }
