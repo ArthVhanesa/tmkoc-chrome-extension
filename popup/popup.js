c6cafdab760d8fff50c9dbd4ffb24953e2df7a2f
Arth Vhanesa <77712031+ArthVhanesa@users.noreply.github.com>
2023-06-12 17:52:42 +0530

    random GIF added

--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,22 @@
 const perfectVideoButton = document.getElementById("perfect-video-button");
+const jethalalGifElement = document.getElementById("jethalal-gif");
 
 perfectVideoButton.addEventListener("click", openRandomVideo);
 
-// console.log("hello popujs");
+const jethalalGif = [
+  "https://media.tenor.com/UUyL8mVNYvMAAAAC/jetha-jethalal.gif",
+  "https://media.tenor.com/tgDbJxp0Yt4AAAAC/jethalal-tarak.gif",
+  "https://media.tenor.com/q2D1TATYTY8AAAAC/jethalal-tmkoc.gif",
+  "https://media.tenor.com/rcUlT6pAH9MAAAAC/jethalal-sab-changa-c.gif",
+  "https://media.tenor.com/rNbOhgMCWOkAAAAC/jethalal-jethalal-dancing.gif",
+  "https://media.tenor.com/W06knTv2yOUAAAAd/jetha-dance.gif",
+  "https://media.tenor.com/jcsfoiV2df4AAAAd/jetha.gif",
+];
+
+const randomIndex = Math.floor(Math.random() * jethalalGif.length);
+
+jethalalGifElement.src = jethalalGif[randomIndex];
+// console.log("hello popup.js");
 
 function openRandomVideo() {
   chrome.storage.local.get("episodes", (result) => {
